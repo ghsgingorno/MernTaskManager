00c8e6f198659adb730b45f703a0f8cb50cb2547
ghsgingorno <93550614+ghsgingorno@users.noreply.github.com>
2024-03-04 10:47:45 +0500

    Model creation and route testing

--- backend/models/TaskModel.js
+++ backend/models/TaskModel.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
-const TaskSchema = new  mongoose.Schema({
+const TaskSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    description: {type:String},
-    completed: { type: Boolean, default: false }
-});
+    description: { type: String },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 // Method to mark a task as complete
 const Tasks = new mongoose.model('Task', TaskSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,36 @@
 import express from "express";
-import connectDB from './config/connectDB.js'
+import connectDB from "./config/connectDB.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Tasks from "./models/TaskModel.js";
 
 const app = express();
-dotenv.config()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
-app.get('/', (req, res) => {
-    res.send("Home Page");
-})
-
+app.post("/api/tasks", async (req, res) => {
+  try {
+    const task = await Tasks.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 const startServer = async () => {
   try {
-    await connectDB()
+    await connectDB();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 startServer();
